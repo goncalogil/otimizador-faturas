c17b769ca74fce8d0f388dfdd5f78d1849bd2f5a
Gonçalo Gil <21153692+goncalogil@users.noreply.github.com>
2024-01-14 22:38:57 +0000

    chore: change time period to full 2023

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { optimizeFaturas } from "./domain/faturasService"
 
 async function main() {
-  const fromDate = new Date('2023-12-01')
+  const fromDate = new Date('2023-01-01')
   const toDate = new Date('2023-12-31')
   await optimizeFaturas(fromDate, toDate)
 }
