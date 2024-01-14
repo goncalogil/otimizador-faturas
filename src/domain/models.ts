e5099536416f75da61483160c2287f40d3d7c223
Gonçalo Gil <21153692+goncalogil@users.noreply.github.com>
2024-01-14 20:37:56 +0000

    feat: update category list

--- src/domain/models.ts
+++ src/domain/models.ts
@@ -9,6 +9,16 @@ export type Fatura = {
 
 
 export enum FaturaClassification {
+  REPARACAO_AUTOMOVEIS = "C01",
+  REPARACAO_MOTOCICLOS = "C02",
+  RESTAURACAO_ALOJAMENTO = "C03",
+  CABELEIREIROS = "C04",
+  SAUDE = "C05",
+  EDUCACAO = "C06",
+  HABITACAO = "C07",
+  LARES = "C08",
+  VETERINARIOS = "C09",
   PASSES = "C10",
+  GINASIOS = "C11",
   OUTROS = "C99"
 }
