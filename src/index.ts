1d48f4cefd8df232549f73ab84ddf89996717ca3
GoncaloGil <goncalo.gil@hotmail.com>
2024-12-21 18:06:32 +0000

    feat: add possiblity to skip optimization of a list of NIFs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { optimizeFaturas } from "./domain/faturasService"
 
 async function main() {
-  const fromDate = new Date('2023-01-01')
-  const toDate = new Date('2023-12-31')
+  const fromDate = new Date('2024-01-01')
+  const toDate = new Date('2024-12-31')
   await optimizeFaturas(fromDate, toDate)
 }
 
